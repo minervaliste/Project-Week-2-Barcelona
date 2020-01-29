8b568db4b715774d8956d73a29fbf875d783576e
Minerva Liste <minerva.liste@gmail.com>
2020-01-29 23:10:26 +0100

    upload correct queries for final DB

--- your-project/Queries_extract_tables_per_district_and_year.sql
+++ your-project/Queries_extract_tables_per_district_and_year.sql
@@ -1,20 +1,22 @@
 USE p2_population;
 
 -- Immigrant age ranges per district and year (percentage)
-SELECT i.year, i.district_name, i.age, ((sum(i.immigrants)/sub_q.total_immigrants)*100) AS immigrants_percentage
-FROM immigrant_age AS i
-LEFT JOIN (SELECT year, district_name, sum(immigrants) as total_immigrants
-FROM immigrant_age
-GROUP BY district_name, year) sub_q ON i.district_name = sub_q.district_name AND i.year = sub_q.year
-GROUP BY i.district_name, i.age, i.year
-ORDER BY i.year DESC, i.district_name, i.age ASC;
+SELECT a.year, sub_q.district_name, a.age, ((sum(a.immigrants)/sub_q.total_immigrants)*100) AS immigrants_percentage
+FROM age AS a
+JOIN (SELECT a1.year, d.neig_code, d.district_name, sum(a1.immigrants) as total_immigrants
+FROM age AS a1
+LEFT JOIN district AS d ON d.neig_code = a1.neig_code
+GROUP BY d.district_name, a1.year) sub_q ON a.neig_code = sub_q.neig_code AND a.year = sub_q.year
+GROUP BY sub_q.district_name, a.age, a.year
+ORDER BY a.year DESC, sub_q.district_name, a.age ASC;
 
 
 -- Immigrant gender per district and year (percentage)
-SELECT i.year, i.district_name, i.gender, ((sum(i.immigrants)/sub_q.total_immigrants)*100) AS immigrants_percentage
-FROM immigrant_gender AS i
-LEFT JOIN (SELECT year, district_name, sum(immigrants) as total_immigrants
-FROM immigrant_gender
-GROUP BY district_name, year) sub_q ON i.district_name = sub_q.district_name AND i.year = sub_q.year
-GROUP BY i.district_name, i.gender, i.year
-ORDER BY i.year DESC, i.district_name, i.gender ASC;
+SELECT g.year, sub_q.district_name, g.gender, ((sum(g.immigrants)/sub_q.total_immigrants)*100) AS immigrants_percentage
+FROM gender AS g
+JOIN (SELECT g1.year, d.neig_code, d.district_name, sum(g1.immigrants) as total_immigrants
+FROM gender AS g1
+LEFT JOIN district AS d ON d.neig_code = g1.neig_code
+GROUP BY d.district_name, g1.year) sub_q ON g.neig_code = sub_q.neig_code AND g.year = sub_q.year
+GROUP BY sub_q.district_name, g.gender, g.year
+ORDER BY g.year DESC, sub_q.district_name, g.gender ASC;
--- your-project/Queries_extract_tables_per_year.sql
+++ your-project/Queries_extract_tables_per_year.sql
@@ -3,28 +3,28 @@ USE p2_population;
 -- EXTRACT: PERCENTAGES OF GENDER DISTRIBUTION IN BARCELONA PER YEAR
 -- Gender distribution per year
 SELECT year, gender, sum(immigrants) as total_immigrants_per_gender
-FROM immigrant_gender
+FROM gender
 GROUP BY gender, year;
 
 -- Immigrants per year
 SELECT year, sum(immigrants) as total_immigrants
-FROM immigrant_gender
+FROM gender
 GROUP BY year;
 
 -- Gender distribution per year (percentage)
 SELECT i.year, i.gender, ((sum(i.immigrants)/ sub_q.total_immigrants)*100) as percentage_immigrants_per_gender
-FROM immigrant_gender AS i
+FROM gender AS i
 LEFT JOIN(SELECT year, sum(immigrants) as total_immigrants
-FROM immigrant_gender
+FROM gender
 GROUP BY year) sub_q ON i.year = sub_q.year
 GROUP BY i.gender, i.year;
 
 -- EXTRACT: PERCENTAGES OF AGE RANGE DISTRIBUTION IN BARCELONA PER YEAR
 -- Age distribution per year (percentage)
 SELECT i.year, i.age, ((sum(i.immigrants)/ sub_q.total_immigrants)*100) as percentage_immigrants_per_gender
-FROM immigrant_age AS i
+FROM age AS i
 LEFT JOIN(SELECT year, sum(immigrants) as total_immigrants
-FROM immigrant_age
+FROM age
 GROUP BY year) sub_q ON i.year = sub_q.year
 GROUP BY i.age, i.year;
 
